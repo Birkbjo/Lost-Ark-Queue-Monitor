659b50db64d3ffbc282250a4556364860200ca48
Birk Johansson <birkbjo@gmail.com>
2022-02-22 15:47:05 +0100

    chore: remove unused deps

--- bin/quickstartSetup.bat
+++ bin/quickstartSetup.bat
@@ -2,9 +2,10 @@
 
 set rootPath=%~dp0..
 
-if not exist %rootPath%\node_modules (
+if not exist %rootPath%\node_modules\package-lock.json (
     echo Installing dependencies...
     call npm install
 )
 
 node %rootPath%\index.js -s
+pause
